197dcecdcae1a534e2bf526371aa4e798b1daf1f
Lucas Correia <88215259+Luskinnn28@users.noreply.github.com>
2021-08-06 18:54:43 -0300

    Update Exercício_5.py

--- Exercício_5.py
+++ Exercício_5.py
@@ -1,4 +1,4 @@
-#15) Escreva um programa que converta uma temperatura digitando em graus Celsius e converta para graus Fahrenheit
+#5) Escreva um programa que converta uma temperatura digitando em graus Celsius e converta para graus Fahrenheit
 
 print("RESOLUÇÃO DA 15")
 
