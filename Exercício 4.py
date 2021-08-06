90390c9fba004885271052ff461727da92d6b266
Lucas Correia <88215259+Luskinnn28@users.noreply.github.com>
2021-08-06 18:50:16 -0300

    Create Exercício 4.py

---
+++ Exercício 4.py
@@ -0,0 +1,11 @@
+#4)Faça um algoritmo que leia o salário de um funcionário e mostre seu novo salário, com 15% de aumento
+
+print("RESOLUÇÃO DA 14")
+
+salarioFixo = float (input('Qual é a sua renda mensal fixa?'))
+
+aumento = salarioFixo + (salarioFixo * 15 / 100)
+
+print("Com um aumento de 15%, seu salário atual é de {}".format(aumento))
+
+print('--------------------------------------------------------------------')
