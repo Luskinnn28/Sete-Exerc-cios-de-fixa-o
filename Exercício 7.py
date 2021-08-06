ba81e8331416aa7858ea4d13952744aed5c4870c
Lucas Correia <88215259+Luskinnn28@users.noreply.github.com>
2021-08-06 18:57:42 -0300

    Create Exercício 7.py

---
+++ Exercício 7.py
@@ -0,0 +1,12 @@
+7) #Um professor quer sortear um dos seus quatro alunos para apagar o quadro. Faça um programa que ajude ele,
+# lendo o nome dos alunos e escrevendo na tela o nome do escolhido.
+
+# Utilizar a biblioteca random.
+
+print('RESOLUÇÃO DA 17')
+
+import random
+
+alunos = ['Lucas Correia', 'Tommy', 'Sérgio', 'Ana Paula']
+
+print('Os participantes são:', alunos)
