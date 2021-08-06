c127522eccb3ce83bfea0cf45fd318e2facc9b11
Lucas Correia <88215259+Luskinnn28@users.noreply.github.com>
2021-08-06 19:20:07 -0300

    Update Exercício 1.py

--- Exercício 1.py
+++ Exercício 1.py
@@ -1,7 +1,7 @@
 #1) Faça um programa que insira um valor em reais, e faça ele converter o valor para dólar,mostrando quantos
 # dólares podem ser comprados com aquela quantia. (Valor para ser usado do dólar no exercício: 5.30)
 
-print('RESOLUÇÃO DA 11')
+print('RESOLUÇÃO DA 1')
 
 ValorEscolhido = float(input("Digite o valor que você quer converter para dolar"))
 
