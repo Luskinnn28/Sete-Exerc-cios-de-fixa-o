50c97cd86b49a13ee3738d58492aac59b47b3e35
Lucas Correia <88215259+Luskinnn28@users.noreply.github.com>
2021-08-06 18:54:07 -0300

    Create Exercício_5.py

---
+++ Exercício_5.py
@@ -0,0 +1,11 @@
+#15) Escreva um programa que converta uma temperatura digitando em graus Celsius e converta para graus Fahrenheit
+
+print("RESOLUÇÃO DA 15")
+
+Graus_Celsius = int(input("Digite a temperatura em Celsius"))
+
+Fareinheit = (9 * Graus_Celsius /5) + 32
+
+print("{}°C equivale a {} °F".format(Graus_Celsius, Fareinheit))
+
+print('------------------------------------------------------------')
