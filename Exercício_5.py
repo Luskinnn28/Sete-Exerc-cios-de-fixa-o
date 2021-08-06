d921b920fb2309d5fcb19d1ce64e47d542455473
Lucas Correia <88215259+Luskinnn28@users.noreply.github.com>
2021-08-06 19:21:23 -0300

    Update Exercício_5.py

--- Exercício_5.py
+++ Exercício_5.py
@@ -1,6 +1,6 @@
 #5) Escreva um programa que converta uma temperatura digitando em graus Celsius e converta para graus Fahrenheit
 
-print("RESOLUÇÃO DA 15")
+print("RESOLUÇÃO DA 5")
 
 Graus_Celsius = int(input("Digite a temperatura em Celsius"))
 
